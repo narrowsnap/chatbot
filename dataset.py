ab84eb769dc7458039b7e20437a0fd4c1137fa7e
yangz <yangz@keyayun.com>
2018-05-11 18:56:31 +0800

    separate big file to lots of small file

--- dataset.py
+++ dataset.py
@@ -1,31 +1,14 @@
 import torch.utils.data as data
-import torch
-import sys
 import numpy as np
-from gensim.models import word2vec
 
 class VecDataSet(data.Dataset):
-    def __init__(self, question, answer, gensim_model, transform):
+    def __init__(self, question, answer):
         self.question = question
         self.answer = answer
-        self.model = word2vec.Word2Vec.load(gensim_model)
-        self.transform = transform
 
     def __getitem__(self, index):
         q, a = self.question[index], self.answer[index]
-        q_vector = []
-        for q_sentence in q:
-            q_word = q_sentence.split(' ')
-
-            q_sentvec = [self.model[w] for w in q_word if w in self.model.wv.vocab]
-            q_vector.append(q_sentvec)
-
-        a_vector = []
-        for a_sentence in a:
-            a_word = a_sentence.split(' ')
-            a_sentvec = [self.model[w] for w in a_word if w in self.model.wv.vocab]
-            a_vector.append(a_sentvec)
-        return self.transform(np.array(q_vector)), self.transform(np.array(a_vector))
+        return q, a
 
     def __len__(self):
         return len(self.question)
--- word.py
+++ word.py
@@ -8,9 +8,15 @@ import numpy as np
 # parser = argparse.ArgumentParser(description='generate ')
 #
 # parser.add_argument('-i', '--input_file', dest='source_file', action='store', required=True, help='source file path')
-# parser.add_argument('-o', '--segment_output', dest='segment_output', action='store', required=True, help='after segment output file path')
+# parser.add_argument('-o', '--segment_output', dest='segment_output',
+# action='store', required=True, help='after segment output file path')
 # parser.add_argument('-g', '--gensim_output', dest='gensim_output', action='store', required=True, help='after gensim output file path')
 # args = parser.parse_args()
+
+PAD = 'PAD'
+EOS = 'EOS'
+UNK = 'UNK'
+GO = 'GO'
 
 class Word:
     def __init__(self,
@@ -28,7 +34,7 @@ class Word:
     def word_segment(self):
 
         # 打开语料文本
-        inputFile_NoSegment = open(self.segment_input, 'rb')
+        inputFile_NoSegment = open(self.segment_input, 'r')
         outputFile_Segment = open(self.segment_output, 'w', encoding='utf-8')
 
         # 读取语料文本中的每一行文字
@@ -38,14 +44,13 @@ class Word:
         for i in range(len(lines)):
             line = lines[i]
             if line:
-                line = line.strip()
+                line = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+", "", line)
                 seg_list = jieba.cut(line)
 
                 segments = ''
                 for word in seg_list:
                     segments = segments + ' ' + word
                 # 为分词后的预料添加结尾
-                segments = segments + ' eos'
                 segments += '\n'
                 segments = segments.lstrip()
 
@@ -62,78 +67,99 @@ class Word:
         model = word2vec.Word2Vec(sentences, min_count=5, size=100)
         model.save(self.gensim_output_model)
 
-    # 将问-答语句输入X-Y中
-    def XY(self):
+    # 将问-答语句输入Q-A中
+    def QA(self):
         # 读取分词后的对话文本
         f = open(self.segment_output, 'r', encoding='utf-8')
         subtitles = f.read()
 
-        X = []
-        Y = []
+        Q = []
+        A = []
 
         # 将对话文本按段落切分
         subtitles_list = subtitles.split('E')
 
-        # 将"问句"放入X中，将‘答句’放入Y中
+        # 将"问句"放入Q中，将‘答句’放入A中
         for q_a in subtitles_list:
-            # 检验段落中，是否含有‘问-答’句， 如果有，则分别追加到X和Y中
+            # 检验段落中，是否含有‘问-答’句， 如果有，则分别追加到Q和A中
             if re.findall('.*M.*M.*', q_a, flags=re.DOTALL):
                 q_a = q_a.strip()
                 q_a_pair = q_a.split('M')
 
-                # X.append(q_a_pair[1].strip())
-                # Y.append(q_a_pair[2].strip())
+                # Q.append(q_a_pair[1].strip())
+                # A.append(q_a_pair[2].strip())
                 for i in range(len(q_a_pair)):
                     if i == 0:
                         continue
                     else:
                         if i%2 == 1:
-                            X.append(q_a_pair[i].strip())
+                            Q.append(q_a_pair[i].strip())
                         else:
-                            Y.append(q_a_pair[i].strip())
-                if len(X) > len(Y):
-                    X.pop()
-                elif len(Y) > len(X):
-                    Y.pop()
+                            A.append(q_a_pair[i].strip())
+                if len(Q) > len(A):
+                    Q.pop()
+                elif len(A) > len(Q):
+                    A.pop()
 
         f.close()
 
-        return X, Y
+        return Q, A
 
-    # 将X和Y中的词语，转换成词向量，并将问答句长度统一
-    def XY_vector(self, X, Y):
-
+    # 将Q和A中的词语，转换成词向量，并将问答句长度统一
+    def QA_vector(self, Q, A):
         # 导入训练好的词向量
         model = word2vec.Word2Vec.load(self.gensim_output_model)
 
-        # 将X-Y转换为词向量X_vector、Y_vector.format(TEMP_FOLDER)
-        X_vector = []
-        for x_sentence in X:
+        # 将Q-A转换为词向量Q_vector、A_vector.format(TEMP_FOLDER)
+        Q_vector = []
+        for x_sentence in Q:
             x_word = x_sentence.split(' ')
 
             x_sentvec = [model[w] for w in x_word if w in model.wv.vocab]
-            X_vector.append(x_sentvec)
+            Q_vector.append(x_sentvec)
 
-        Y_vector = []
-        for y_sentence in Y:
+        A_vector = []
+        for y_sentence in A:
             y_word = y_sentence.split(' ')
             y_sentvec = [model[w] for w in y_word if w in model.wv.vocab]
-            Y_vector.append(y_sentvec)
+            A_vector.append(y_sentvec)
 
-        # 计算词向量的维数
-        word_dim = len(X_vector[1][0])
+            # 计算词向量的维数
+        word_dim = len(Q_vector[1][0])
 
         # 设置结束词
         sentend = np.ones((word_dim,), dtype=np.float32)
 
-        # 给问-答句添加结尾
-        for sentvec in X_vector:
-            sentvec.append(sentend)
+        # 将问-答句的长度统一
+        for sentvec in Q_vector:
+            if len(sentvec) > 14:
+                # 将第14个词之后的全部内容删除，并将第15个词换为sentend
+                sentvec[14:] = []
+                sentvec.append(sentend)
+            else:
+                # 将不足15个词的句子，用sentend补足
+                for i in range(15 - len(sentvec)):
+                    sentvec.append(sentend)
 
-        for sentvec in Y_vector:
-            sentvec.append(sentend)
+        for sentvec in A_vector:
+            if len(sentvec) > 15:
+                sentvec[14:] = []
+                sentvec.append(sentend)
+            else:
+                for i in range(15 - len(sentvec)):
+                    sentvec.append(sentend)
 
-        return X_vector, Y_vector
+        return Q_vector, A_vector
+
+    # 将QA vector 拆分成小文件
+    def piece_qa_vector(self, q_v, a_v):
+        count = 0
+        for i in range(len(q_v)):
+            if i % 2 == 0:
+                np.save('data/train/q_'+str(count)+'.npy', q_v[count*2:i])
+                np.save('data/train/a_'+str(count)+'.npy', a_v[count*2:i])
+                count += 1
+
 
 def test():
     word = Word(
@@ -144,8 +170,14 @@ def test():
     )
     # word.word_segment()
     # word.word_model()
-    q, a = word.XY()
-
+    # Q, A = word.QA()
+    # word.deal_with_qa(Q, A)
+    # q_v, a_v = word.QA_vector(Q, A)
+    q_v = [[1, 2, 3], [2, 3, 4], [3, 4, 5], [1, 2, 3], [2, 3, 4], [3, 4, 5], [3, 4, 5]]
+    a_v = [[1, 2, 3], [2, 3, 4], [3, 4, 5], [1, 2, 3], [2, 3, 4], [3, 4, 5], [3, 4, 5]]
+    q_v = np.array(q_v)
+    a_v = np.array(a_v)
+    word.piece_qa_vector(q_v, a_v)
 
 if __name__ == '__main__':
     test()
