72fed620048279b33fa68594d4f078eb0990d0e2
narrowsnap <13121155857@163.com>
2018-05-12 17:48:51 +0800

    rewrite train, implement a simple encoder decoder train function

--- train.py
+++ train.py
@@ -10,168 +10,82 @@ import time
 import math
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-import numpy as np
 import seq2seq_model as model
 import word
+import dataset
+import torch.utils.data as data
+import argparse
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    '--hidden_size',
+    dest='hidden_size',
+    action='store',
+    required=True,
+    help='lstm hidden size')
+
+parser.add_argument(
+    '--learning_rate',
+    dest='hidden_size',
+    action='store',
+    required=True,
+    help='learning_rate')
+
+parser.add_argument(
+    '--batch_size',
+    dest='batch_size',
+    action='store',
+    required=True,
+    help='batch size')
+
+parser.add_argument(
+    '--num_layers',
+    dest='num_layers',
+    action='store',
+    required=True,
+    help='lstm 层数')
+
+args = parser.parse_args()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-teacher_forcing_ratio = 1
-MAX_LENGTH = 10
-EOS_token = 'eos'
-SOS_token = 'sos'
 
-def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer,
-          decoder_optimizer, criterion, max_length=MAX_LENGTH):
-    encoder_hidden = encoder.initHidden()
-
-    encoder_optimizer.zero_grad()
-    decoder_optimizer.zero_grad()
-
-    input_length = input_tensor.size(0)
-    target_length = target_tensor.size(0)
-
-    # encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
-
-    loss = 0
-
-    for ei in range(input_length):
-        encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
-        # encoder_outputs[ei] = encoder_output[0, 0]
-
-    decoder_input = encoder_output
-
-    decoder_hidden = encoder_hidden
-
-    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
-
-    if use_teacher_forcing:
-        # Teacher forcing: Feed the target as the next input
-        for di in range(target_length):
-            decoder_output, decoder_hidden, decoder_attention = decoder(
-                decoder_input, decoder_hidden
-            )
-            loss += criterion(decoder_output, target_tensor[di])
-            decoder_input = target_tensor[di]
-
-    else:
-        # Without teacher forcing: use its own predictions as the next input
-        for di in range(target_length):
-            decoder_output, decoder_hidden, decoder_attention = decoder(
-                decoder_input, decoder_hidden
-            )
-            topv, topi = decoder_output.topk(1) # detach from history as input
-
-            loss += criterion(decoder_output, target_tensor[di])
-            if decoder_input.item() == EOS_token:
-                break
-    loss.backward()
-
-    encoder_optimizer.step()
-    decoder_optimizer.step()
-
-    return loss.item() / target_length
-
-def generator():
+def train():
     w = word.Word(
         'data/dgk_shooter.conv',
         'data/dgk_segment.conv',
         'data/dgk_segment.conv',
         'model/dgk_gensim_model'
     )
-    q, a = w.XY()
+    q, a = w.QA()
+    generate = w.generate_vector(q, a)
+    i, q_v, a_v = next(generate)    # 生成数据, i用于判断结束
+    ds = dataset.VecDataSet(q_v, a_v)
+    train_loader = data.DataLoader(ds, batch_size=64, shuffle=True)
+    encoder = model.EncoderRNN(q_v.shape[2], args.hidden_size, q_v.shape[2], args.num_layers, args.batch_size).to(device)
+    decoder = model.DecoderRNN(q_v.shape[2], args.hidden_size, q_v.shape[2], args.num_layers, args.batch_size).to(device)
+    encoder_optimizer = optim.Adam(encoder.parameters(), lr=args.learning_rate)
+    decoder_optimizer = optim.Adam(decoder.parameters(), lr=args.learning_rate)
+    loss_func = nn.CrossEntropyLoss()
+    for i, (x, y) in enumerate(train_loader):
+        x = x.to(device)
+        y = y.to(device)
+        h0 = encoder.initHidden()
+        output, hidden = encoder(x, (h0, h0))
+        # 把encoder的输出作为decoder的第一个输入
+        y = y[:, :-1, :]    # 去掉y最后一个词，也可以在生成数据时把y少填充一个
+        y = torch.cat((output, y), 1)
+        h0 = decoder.initHidden()
+        output, hidden = decoder(y, (h0, h0))
+        loss = loss_func(output, y)
+        encoder_optimizer.zero_grad()
+        decoder_optimizer.zero_grad()
+        loss.backward()
+        encoder_optimizer.step()
+        decoder_optimizer.step()
 
-def as_minutes(s):
-    m = math.floor(s / 60)
-    s -= m * 69
-    return '%dm %ds' % (m, s)
-
-def time_since(since, percent):
-    now = time.time()
-    s = now - since
-    es = s / (percent)
-    rs = es - s
-    return '%s (- %s)' % (as_minutes(s), as_minutes(rs))
-
-def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100, learning_rate=0.01):
-    start = time.time()
-    plot_losses = []
-    print_loss_total = 0    # Reset every print_every
-    plot_loss_total = 0     # Reset every plot_every
-
-    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
-    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
-    training_pairs = [tensorsFromPair(random.choice())]
-    criterion = nn.NLLLoss()
-
-    for iter in range(1, n_iters+1):
-        training_pair = training_pairs[iter - 1]
-        input_tensor = training_pair[0]
-        target_tensor = training_pair[1]
-
-        loss = train(input_tensor, target_tensor, encoder,
-                     decoder, encoder_optimizer, decoder_optimizer, criterion)
-
-        print_loss_total += loss
-        plot_loss_total += loss
-
-        if iter % print_every == 0:
-            print_loss_avg = print_loss_total / print_every
-            print_loss_total = 0
-            print('%s (%d %d%%) %.4f' % (time_since(start, iter/n_iters),
-                                         iter, iter/n_iters*100, print_loss_avg))
-
-        if iter % plot_every == 0:
-            plot_loss_avg = plot_loss_total / plot_every
-            plot_losses.append(plot_loss_avg)
-            plot_loss_total = 0
-
-    show_plot(plot_losses)
-
-def show_plot(points):
-    plt.figure()
-    fig, ax = plt.subplots()
-    # this locator puts ticjs at regular intervals
-    loc = ticker.MultipleLocator(base=0.2)
-    ax.yaxis.set_major_locator(loc)
-    plt.plot(points)
-
-def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
-    with torch.no_grad():
-        input_tensor = tensorFromSentence(input_lang,sentence)
-        input_length = input_tensor.size()[0]
-        encoder_hidden = encoder.initHidden()
-
-        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=decoder)
-
-        for ei in range(input_length):
-            encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
-            encoder_outputs[ei] += encoder_output[0, 0]
-
-        decoder_input = torch.tensor([[SOS_token]], device=device)
-        decoder_hidden = encoder_hidden
-
-        decoder_words = []
-        decoder_attentions = torch.zeros(max_length, max_length)
-
-        for di in range(max_length):
-            decoder_output, decoder_hidden, decoder_attention = decoder(
-                decoder_input, decoder_hidden, encoder_outputs
-            )
-            decoder_attentions[di] = decoder_attention.data
-            topv, topi = decoder_output.data.topk(1)
-            if topi.item() == EOS_token:
-                decoder_words.append('eos')
-                break
-            else:
-                decoder_words.append()
-
-            decoder_input = topi.squeeze().detach()
-
-        return decoder_words, decoder_attention[: di+1]
+def test():
+    print('test')
 
 if __name__ == '__main__':
-    hidden_size = 1024
-    encoder1 = model.EncoderRNN(100, hidden_size).to(device)
-    attn_decoder1 = model.AttnDecoderRNN(hidden_size, 100, dropout_p=0.1).to(device)
+    train()
 
-    trainIters(encoder1, attn_decoder1, 75000, print_every=5000)
